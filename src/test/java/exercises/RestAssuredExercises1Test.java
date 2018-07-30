b59437fedf180665621de88ff5124d47047fa86a
oocl-vito <hellozojian@gmail.com>
2018-07-30 11:19:00 +0800

    check the array contain appointed item

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -9,121 +9,126 @@ import io.restassured.module.jsv.JsonSchemaValidator;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 
 
 public class RestAssuredExercises1Test {
 
-    private static RequestSpecification requestSpec;
+	private static RequestSpecification requestSpec;
 
-    @BeforeAll
-    public static void createRequestSpecification() {
+	@BeforeAll
+	public static void createRequestSpecification() {
 
-        requestSpec = new RequestSpecBuilder().
-                setBaseUri("http://localhost").
-                setPort(9876).
-                setBasePath("/api/f1").
-                build();
-    }
+		requestSpec = new RequestSpecBuilder().
+				setBaseUri("http://localhost").
+				setPort(9876).
+				setBasePath("/api/f1").
+				build();
+	}
 
-    /*******************************************************
-     * Send a GET request to /2016/drivers.json
-     * and check that the response has HTTP status code 200
-     ******************************************************/
+	/*******************************************************
+	 * Send a GET request to /2016/drivers.json
+	 * and check that the response has HTTP status code 200
+	 ******************************************************/
 
-    @Test
-    public void checkResponseCodeForCorrectRequest() {
+	@Test
+	public void checkResponseCodeForCorrectRequest() {
 
-        given().
-                spec(requestSpec).
-                when().
-		        get("/2016/drivers.json").
-                then().
-		        assertThat().
-		        statusCode(200);
-    }
+		given().
+				spec(requestSpec).
+				when().
+				get("/2016/drivers.json").
+				then().
+				assertThat().
+				statusCode(200);
+	}
 
-    /*******************************************************
-     * Send a GET request to /incorrect.json
-     * and check that the answer has HTTP status code 404
-     ******************************************************/
+	/*******************************************************
+	 * Send a GET request to /incorrect.json
+	 * and check that the answer has HTTP status code 404
+	 ******************************************************/
 
-    @Test
-    public void checkResponseCodeForIncorrectRequest() {
+	@Test
+	public void checkResponseCodeForIncorrectRequest() {
 
-        given().
-                spec(requestSpec).
-                when().
-		        get("/incorrect.json").
-                then().
-		        assertThat().
-		        statusCode(404);
-    }
+		given().
+				spec(requestSpec).
+				when().
+				get("/incorrect.json").
+				then().
+				assertThat().
+				statusCode(404);
+	}
 
-    /*******************************************************
-     * Send a GET request to /2016/drivers.json
-     * and check that the response is in JSON format
-     ******************************************************/
+	/*******************************************************
+	 * Send a GET request to /2016/drivers.json
+	 * and check that the response is in JSON format
+	 ******************************************************/
 
-    @Test
-    public void checkResponseContentTypeJson() {
+	@Test
+	public void checkResponseContentTypeJson() {
 
-        given().
-                spec(requestSpec).
-                when().
-		        get("/2016/drivers.json").
-                then().
-		        contentType(ContentType.JSON);
-    }
+		given().
+				spec(requestSpec).
+				when().
+				get("/2016/drivers.json").
+				then().
+				contentType(ContentType.JSON);
+	}
 
-    /***********************************************
-     * Retrieve circuit information for the first race
-     * of the 2014 season and check the circuitId equals
-     * albert_park
-     * Use /2014/1/circuits.json
-     **********************************************/
+	/***********************************************
+	 * Retrieve circuit information for the first race
+	 * of the 2014 season and check the circuitId equals
+	 * albert_park
+	 * Use /2014/1/circuits.json
+	 **********************************************/
 
-    @Test
-    public void checkTheFirstRaceOf2014WasAtAlbertPark() {
+	@Test
+	public void checkTheFirstRaceOf2014WasAtAlbertPark() {
 
-        given().
-                spec(requestSpec).
-                when().
-		        get("/2014/1/circuits.json").
-                then().
-		        log().
-		        all().
-		        body("MRData.CircuitTable.Circuits[0].circuitId",is("albert_park"));
-    }
+		given().
+				spec(requestSpec).
+				when().
+				get("/2014/1/circuits.json").
+				then().
+				log().
+				all().
+				body("MRData.CircuitTable.Circuits[0].circuitId", is("albert_park"));
+	}
 
-    /***********************************************
-     * Retrieve the list of circuits for the 2014
-     * season and check that it contains silverstone
-     * Use /2014/circuits.json
-     **********************************************/
+	/***********************************************
+	 * Retrieve the list of circuits for the 2014
+	 * season and check that it contains silverstone
+	 * Use /2014/circuits.json
+	 **********************************************/
 
-    @Test
-    public void checkThereWasARaceAtSilverstoneIn2014() {
+	@Test
+	public void checkThereWasARaceAtSilverstoneIn2014() {
 
-        given().
-                spec(requestSpec).
-                when().
-                then();
-    }
+		given().
+				spec(requestSpec).
+				when().
+				get("/2014/circuits.json").
+				then().
+				log().
+				all().
+				body("MRData.CircuitTable.Circuits.circuitId", hasItems("silverstone"));
+	}
 
-    /***********************************************
-     * Retrieve the list of circuits for the 2014
-     * season and check that it does not contain
-     * nurburgring
-     * USe /2014/circuits.json
-     **********************************************/
+	/***********************************************
+	 * Retrieve the list of circuits for the 2014
+	 * season and check that it does not contain
+	 * nurburgring
+	 * USe /2014/circuits.json
+	 **********************************************/
 
-    @Test
-    public void checkThereWasNoRaceAtNurburgringIn2014() {
+	@Test
+	public void checkThereWasNoRaceAtNurburgringIn2014() {
 
-        given().
-                spec(requestSpec).
-                when().
+		given().
+				spec(requestSpec).
+				when().
 
-                then();
-    }
+				then();
+	}
 }
