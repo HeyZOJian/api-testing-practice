a6904355bf7d22f5d87bcda410c2459dcc3cc479
oocl-vito <hellozojian@gmail.com>
2018-07-30 10:59:56 +0800

    check response status code is 200 for correct request

--- src/test/java/exercises/RestAssuredExercises1Test.java
+++ src/test/java/exercises/RestAssuredExercises1Test.java
@@ -33,7 +33,10 @@ public class RestAssuredExercises1Test {
         given().
                 spec(requestSpec).
                 when().
-                then();
+		        get("/2016/drivers.json").
+                then().
+		        assertThat().
+		        statusCode(200);
     }
 
     /*******************************************************
